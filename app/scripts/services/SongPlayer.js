689b1b832d03a2007cbc3b509b76739857c50eb0
alish95 <alicewc95@gmail.com>
2017-08-20 20:35:52 -0700

    checkpoint-8

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,12 +1,12 @@
 (function() {
-  function SongPlayer() {
+  function SongPlayer(Fixtures) {
     var SongPlayer = {};
 
 /*
-@desc currentSong object
+@desc Store current album info
 @type {Object}
 */
-    var currentSong = null;
+    var currentAlbum = Fixtures.getAlbum();
 
 /*
 @desc Buzz object audio file
@@ -22,8 +22,7 @@
 
     var setSong = function(song) {
       if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        currentSong.playing = null;
+        stopSong(SongPlayer.currentSong);
       }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -31,9 +30,24 @@
         preload: true
       });
 
-// Set the newly chosen song object as the currentSong
-      currentSong = song;
+// Set the newly chosen song object as the SongPlayer.currentSong
+      SongPlayer.currentSong = song;
     }
+
+/*
+@function getSongIndex
+@desc Gets the index of a song
+@param {Object} song
+*/
+    var getSongIndex = function(song) {
+      return currentAlbum.songs.indexOf(song);
+    };
+
+/*
+@desc SongPlayer.currentSong object
+@type {Object}
+*/
+    SongPlayer.currentSong = null;
 
 /*
 @function playSong
@@ -46,19 +60,31 @@
     }
 
 /*
+@function stopSong
+@desc Stops playing currentBuzzObject and sets song playing boolean to false
+@param {Object} song
+*/
+
+    var stopSong = function(song) {
+      currentBuzzObject.stop();
+      song.playing = null;
+    }
+
+/*
 @function SongPlayer.play
 @desc Checks if currently playing song is not the song that the user clicks on
 and calls setSong function if true
 @param {Object} song
 */
     SongPlayer.play = function(song) {
-      if (currentSong !== song) {
+      song = song || SongPlayer.currentSong;
+      if (SongPlayer.currentSong !== song) {
         setSong(song);
         playSong(song);
 
 /* if currently playing song is the song that the user clicks on, then it is
 probably paused and the user would like to resume playing it */
-      } else if (currentSong === song) {
+      } else if (SongPlayer.currentSong === song) {
         if (currentBuzzObject.isPaused()) {
           currentBuzzObject.play();
         }
@@ -71,9 +97,46 @@ probably paused and the user would like to resume playing it */
 @param {Object} song
 */
     SongPlayer.pause = function(song) {
+      song = song || SongPlayer.currentSong;
       currentBuzzObject.pause();
       song.playing = false;
     };
+
+/*
+@function SongPlayer.previous
+@desc Goes to the previous song by getting the index of the current song and
+decreasing by 1
+*/
+    SongPlayer.previous = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex--;
+
+      if (currentSongIndex < 0) {
+        stopSong(SongPlayer.currentSong);
+      } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
+    };
+
+/*
+@function SongPlayer.next
+@desc Goes to the next song by getting the index of the current song and
+increasing by 1
+*/
+    SongPlayer.next = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex++;
+
+      if (currentSongIndex >= currentAlbum.songs.length) {
+        stopSong(SongPlayer.currentSong);
+      } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
+    }
 
     return SongPlayer;
   }
